59c1b99196263d238f2b9eeb5b8e5648a6243271
Pedro Henrique <pedro.souzasilva2209>
2025-02-15 16:51:14 -0300

    Infra Tratador de Erros Parte 3, Error 404

--- remedios/src/main/java/dev/br/project/remedios/Infra/TratadorDeErros.java
+++ remedios/src/main/java/dev/br/project/remedios/Infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package dev.br.project.remedios.Infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 //Toda vez que o spring não achar o Id como por exemplo nunca foi criado, ele vai retornar o 404 pois não existe. m
@@ -12,4 +14,17 @@ public class TratadorDeErros {
     public ResponseEntity<?> tratador404(){
  return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> tratador400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();//pega todos os campos que deram erros
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+    }
+//criando um dto dentro desta classe pois ele so vai ser usado dentro desta classe
+    public record DadosErros(String campo,String messagem){
+//parametros
+        public DadosErros(FieldError error){
+            this(error.getField(),error.getDefaultMessage());
+        }
+}
 }
